c1ddddfafcf1f10145f81a3fb06b4698356702d5
Ali Knayber <79058397+Ali-Knayber@users.noreply.github.com>
2021-11-19 01:52:16 +0200

    Add files via upload
    start backend

---
+++ tutor_register.php
@@ -0,0 +1,8 @@
+<?php
+$first_name = $_POST["first_name"];
+$last_name = $_POST["last_name"];
+$age = $_POST["age"];
+$phone_number = $_POST["phone_number"];
+$email_address = $_POST["email_address"];
+print_r($_POST);
+?>
