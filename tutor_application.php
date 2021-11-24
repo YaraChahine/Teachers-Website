d47e060ebcd9ed933d171b38e665783d3b30bb24
Ali Knayber <79058397+Ali-Knayber@users.noreply.github.com>
2021-11-24 09:50:49 +0200

    fetch part 1 + fixed name education_level

--- tutor_application.php
+++ tutor_application.php
@@ -1,8 +1,15 @@
 <?php
 $first_name = $_POST["first_name"];
 $last_name = $_POST["last_name"];
-$age = $_POST["age"];
-$phone_number = $_POST["phone_number"];
 $email_address = $_POST["email_address"];
+$password = $_POST["password"];
+$age = $_POST["age"];
+$gender = $_POST["gender"];
+$phone_number = $_POST["phone_number"];
+$city = $_POST["city"];
+$years = $_POST["years"];
+$education_level = $_POST["education_level"];
+$school = $_POST["school"];
+$years = $_POST["years"];
 print_r($_POST);
 ?>
