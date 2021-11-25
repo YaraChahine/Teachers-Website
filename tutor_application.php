aa2f1d7331920b9f864a13844ad45ca4640c6adc
Ali Knayber <79058397+Ali-Knayber@users.noreply.github.com>
2021-11-25 10:19:54 +0200

    changed back to text in input + fetching data(tutor_application) + deleted required temporarily for testing purposes

--- tutor_application.php
+++ tutor_application.php
@@ -1,15 +1,104 @@
 <?php
-$first_name = $_POST["first_name"];
-$last_name = $_POST["last_name"];
-$email_address = $_POST["email_address"];
-$password = $_POST["password"];
-$age = $_POST["age"];
-$gender = $_POST["gender"];
-$phone_number = $_POST["phone_number"];
-$city = $_POST["city"];
-$years = $_POST["years"];
-$education_level = $_POST["education_level"];
-$school = $_POST["school"];
-$years = $_POST["years"];
-print_r($_POST);
+if(isset($_POST["first_name"]) && $_POST["first_name"]!=""){
+    $first_name = $_POST["first_name"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["last_name"]) && $_POST["last_name"]!=""){
+    $last_name = $_POST["last_name"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["email_address"]) && $_POST["email_address"]!=""){
+    $email_address = $_POST["email_address"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["password"]) && $_POST["password"]!=""){
+    $password = hash("sha256",$_POST["password"]);
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["age"]) && $_POST["age"]!=""){
+    $age = $_POST["age"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["gender"]) && $_POST["gender"]!=""){
+    $gender = $_POST["gender"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["phone_number"]) && $_POST["phone_number"]!=""){
+    $phone_number = $_POST["phone_number"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["city"]) && $_POST["city"]!=""){
+    $city = $_POST["city"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["years"]) && $_POST["years"]!=""){
+    $years = $_POST["years"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["education_level"]) && $_POST["education_level"]!=""){
+    $education_level = $_POST["education_level"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["school"]) && $_POST["school"]!=""){
+    $school = $_POST["school"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["years"]) && $_POST["years"]!=""){
+    $years = $_POST["years"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["course1-input"]) && $_POST["course1-input"]!=""){
+    $course1_select = $_POST["course1-input"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["course1-level"]) && $_POST["course1-level"]!=""){
+    $course1_level = $_POST["course1-level"];
+}else{
+    die("Nice try Dr. :D ");
+}
+
+if($_POST["course2-input"]!=""){
+    $course2_level = $_POST["course2-input"];
+    $course2_select = $_POST["course2-level"];
+}
+if($_POST["course3-input"]!=""){
+    $course3_level = $_POST["course3-input"];
+    $course3_select = $_POST["course3-level"];
+}
+if($_POST["course4-input"]!=""){
+    $course4_level = $_POST["course4-input"];
+    $course4_select = $_POST["course4-level"];
+}
+if(isset($_POST["cv_file"]) && $_POST["cv_file"]!=""){
+    $cv_file = $_POST["cv_file"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["img_file"]) && $_POST["img_file"]!=""){
+    $img_file = $_POST["img_file"];
+}else{
+    die("Nice try Dr. :D ");
+}
+if(isset($_POST["bio"]) && $_POST["bio"]!=""){
+    $bio = $_POST["bio"];
+}else{
+    die("Nice try Dr. :D ");
+}
+
+
+die($password)
+// print_r($_POST);
 ?>
