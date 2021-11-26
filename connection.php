b0f683548e39307b29d2a562bb3930aa1bb8edd5
Ali Knayber <79058397+Ali-Knayber@users.noreply.github.com>
2021-11-26 20:55:02 +0200

    Validation step 2

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "teachersdatabase";
+
+$connection = new mysqli($server, $username, $password, $dbname);
+
+
+if($connection->connect_error){
+	die("Failed");
+}
+print_r("Connected successfully");
+
+?>
