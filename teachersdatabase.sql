5168d1d65a62990a3a3f1d15321032f905003060
Ali Knayber <79058397+Ali-Knayber@users.noreply.github.com>
2021-11-28 09:59:00 +0200

    correct db

--- teachersdatabase.sql
+++ teachersdatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2021 at 12:46 AM
+-- Generation Time: Nov 28, 2021 at 08:57 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -20,6 +20,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `teachersdatabase`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `booking_number` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `starting_date` date NOT NULL,
+  `days_of_sessions` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `calendar`
+--
+
+CREATE TABLE `calendar` (
+  `item_id_calendar` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_name_calendar` varchar(250) NOT NULL,
+  `importance` varchar(250) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -52,6 +81,13 @@ CREATE TABLE `pending_students` (
   `days_of_sessions` varchar(250) NOT NULL,
   `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pending_students`
+--
+
+INSERT INTO `pending_students` (`temp_student_id`, `first_name`, `last_name`, `phone_number`, `email`, `education_level_student`, `course`, `preferred_tutor`, `starting_date`, `days_of_sessions`, `price`) VALUES
+(1, 'asa', 'sa', 'aa', 'ali.sa@hotmail.com', 'sd', 'sd', 's', '0000-00-00', 's', 0);
 
 -- --------------------------------------------------------
 
@@ -102,7 +138,7 @@ INSERT INTO `pending_tutors` (`temp_user_id`, `first_name`, `last_name`, `email`
 (10, 'alii', 'sg', 'ali.knayber@lau.edu', '098ed0a6477db3aee54048eb7262d133687420bab7a75688b291c1157e64467b', 2112, 'male', '1121111', 'wDSA', 'highschool-degree', 'alii sg', '212', 1212, 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'hi.pdf', 'hi.png', '1221'),
 (11, 'alii', 'sg', 'ali.knayber@hotmail.com', '89aa1e580023722db67646e8149eb246c748e180e34a1cf679ab0b41a416d904', 2112, 'male', '1121111', 'wDSA', 'highschool-degree', 'alii sg', '212', 1212, 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'hi.pdf', 'hi.png', '1221'),
 (12, 'alii', 'sgasas', 'ali.knayber@hotmail.com', 'f527d985dacc01f6384f25a69b8e03d83c35e78294ea73e10eb9a75e88bc6b83', 31, 'male', '1121111', 'wDSA', 'highschool-degree', 'grss sg', '3434', 3, 'Math', 'primary', 'Math', 'primary', 'houg', 'college', 'ok', 'college', 'hi.pdf', 'hi.png', 'ddf'),
-(13, 'alii', 'sgasas', 'ali.knayber@hotmail.com', '96bb5ece92649c554f0503e0d4e3463dbf195a13cadee3b760a3a09da1abd18a', 32, 'male', '1121111', 'wDSA', 'highschool-degree', 'grss sg', '3434', 3, 'Math', 'primary', 'Math', 'primary', 'houg', 'college', 'ok', 'college', 'hi.pdf', 'hi.png', 'ddf'),
+(13, 'alii', 'sgasas', 'ali.knayber1@hotmail.com', '96bb5ece92649c554f0503e0d4e3463dbf195a13cadee3b760a3a09da1abd18a', 32, 'male', '1121111', 'wDSA', 'highschool-degree', 'grss sg', '3434', 3, 'Math', 'primary', 'Math', 'primary', 'houg', 'college', 'ok', 'college', 'hi.pdf', 'hi.png', 'ddf'),
 (14, 'alii', 'sg', 'ali.knayber@lau.edu', '96bb5ece92649c554f0503e0d4e3463dbf195a13cadee3b760a3a09da1abd18a', 0, 'male', '1121111', 'wDSA', 'highschool-degree', 'alii sg', '112', 12, 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'hi.pdf', 'hi.png', 'a'),
 (15, 'alii', 'sg', 'ali.knayber@lau.edu', '96bb5ece92649c554f0503e0d4e3463dbf195a13cadee3b760a3a09da1abd18a', 1213, 'male', '1121111', 'wDSA', 'highschool-degree', 'alii sg', '112', 12, 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'hi.pdf', 'hi.png', 'a'),
 (16, 'aliias', 'sg', 'ali.knayber@lau.edu', '96bb5ece92649c554f0503e0d4e3463dbf195a13cadee3b760a3a09da1abd18a', 1213, 'male', '1121212', 'wDSA', 'highschool-degree', 'alii sg', '112', 12, 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'hi.pdf', 'hi.png', 'a'),
@@ -114,7 +150,12 @@ INSERT INTO `pending_tutors` (`temp_user_id`, `first_name`, `last_name`, `email`
 (22, 'alii', 'sg', 'ali.knayber@lau.edu', '96bb5ece92649c554f0503e0d4e3463dbf195a13cadee3b760a3a09da1abd18a', 18, 'Other', '1121111', 'wDSA', 'highschool-degree', 'alii as sg', '3434', 121, 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'hi.pdf', 'hi.png', 'a'),
 (23, 'alii', 'sg', 'ali.knayber@lau.edu', '96bb5ece92649c554f0503e0d4e3463dbf195a13cadee3b760a3a09da1abd18a', 60, 'Other', '1121111', 'wDSA', 'highschool-degree', 'alii as sg', '3434', 121, 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'hi.pdf', 'hi.png', 'a'),
 (24, 'grss', 'sg', 'ali.knayber@lau.edu', '96bb5ece92649c554f0503e0d4e3463dbf195a13cadee3b760a3a09da1abd18a', 30, 'Male', '1121111', 'wDSA', 'Highschool degree', 'grss sg', 'ss', 2, 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'hi.pdf', 'hi.png', 'a'),
-(25, 'grss', 'sg', 'ali.knayber@lau.edu', '4f15e62e98d0eb3f530af0c46bd319235df916c2dda16824bb7d92dd07275196', 20, 'other', '1121111', 'wDSA', 'undergrad', 'grss sg', 'gs', 1, 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'hi.pdf', 'hi.pdf', 'a');
+(25, 'grss', 'sg', 'ali.knayber@lau.edu', '4f15e62e98d0eb3f530af0c46bd319235df916c2dda16824bb7d92dd07275196', 20, 'other', '1121111', 'wDSA', 'undergrad', 'grss sg', 'gs', 1, 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'Math', 'primary', 'hi.pdf', 'hi.pdf', 'a'),
+(26, 'test', 'sg', 'ali.kk@hotmail.com', '4f15e62e98d0eb3f530af0c46bd319235df916c2dda16824bb7d92dd07275196', 31, 'Male', '1121111', 'wDSA', '0', 'grss sg', 'gs', 3, 'Chemistry', 'primary', 'none', 'none', 'none', 'none', 'none', 'none', '', '.png', 'a'),
+(27, 'test', 'sg', 'ali.sas@hotmail.com', '4f15e62e98d0eb3f530af0c46bd319235df916c2dda16824bb7d92dd07275196', 31, 'Male', '1121111', 'wDSA', '0', 'grss sg', 'gs', 3, 'Chemistry', 'primary', 'none', 'none', 'none', 'none', 'none', 'none', '.pdf', '.png', 'a'),
+(28, 'test', 'sg', 'ali.sasaa@hotmail.com', '4f15e62e98d0eb3f530af0c46bd319235df916c2dda16824bb7d92dd07275196', 31, 'Male', '1121111', 'wDSA', '0', 'grss sg', 'gs', 3, 'Chemistry', 'primary', 'none', 'none', 'none', 'none', 'none', 'none', '.pdf', '', 'a'),
+(29, 'test', 'sg', 'ali.dfdaf@hotmail.com', '4f15e62e98d0eb3f530af0c46bd319235df916c2dda16824bb7d92dd07275196', 31, 'Male', '1121111', 'wDSA', '0', 'grss sg', 'gs', 3, 'Chemistry', 'primary', 'none', 'none', 'none', 'none', 'none', 'none', '', '.png', 'a'),
+(30, 'test', 'sg', 'ali.dfdaf@hotmail.coma', '4f15e62e98d0eb3f530af0c46bd319235df916c2dda16824bb7d92dd07275196', 31, 'Male', '1121111', 'wDSA', '0', 'grss sg', 'gs', 3, 'Chemistry', 'primary', 'none', 'none', 'none', 'none', 'none', 'none', '', '.png', 'a');
 
 -- --------------------------------------------------------
 
@@ -126,6 +167,33 @@ CREATE TABLE `students` (
   `student_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `price_range` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp_codes`
+--
+
+CREATE TABLE `temp_codes` (
+  `code_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `code` varchar(250) NOT NULL,
+  `created` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `to_do_list`
+--
+
+CREATE TABLE `to_do_list` (
+  `item_id_todolist` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_name_todolist` varchar(250) NOT NULL,
+  `flag` tinyint(1) NOT NULL,
+  `line_through` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -174,9 +242,45 @@ INSERT INTO `users` (`user_id`, `type`, `first_name`, `last_name`, `email`, `pho
 (2, 2, 'alis', 'ad', 'ali.knayber1@edu.edu', '3232', '4f15e62e98d0eb3f530af0c46bd319235df916c2dda16824bb7d92dd07275196'),
 (3, 3, 'adsas', 'ads', 'ali.knayber3@edu.edu', '3232', '12');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_types`
+--
+
+CREATE TABLE `user_types` (
+  `type` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_types`
+--
+
+INSERT INTO `user_types` (`type`, `name`) VALUES
+(1, 'Admin'),
+(2, 'Tutor'),
+(3, 'Student');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`booking_number`),
+  ADD KEY `fk_foreign_key_name1` (`course_id`),
+  ADD KEY `fk_foreign_key_name3` (`tutor_id`),
+  ADD KEY `fk_foreign_key_name4` (`student_id`);
+
+--
+-- Indexes for table `calendar`
+--
+ALTER TABLE `calendar`
+  ADD PRIMARY KEY (`item_id_calendar`),
+  ADD KEY `Foreign_Key` (`user_id`);
 
 --
 -- Indexes for table `courses`
@@ -204,6 +308,20 @@ ALTER TABLE `students`
   ADD KEY `fk_foreign_key_name` (`user_id`);
 
 --
+-- Indexes for table `temp_codes`
+--
+ALTER TABLE `temp_codes`
+  ADD PRIMARY KEY (`code_id`),
+  ADD KEY `FOREIGN_KEY` (`user_id`);
+
+--
+-- Indexes for table `to_do_list`
+--
+ALTER TABLE `to_do_list`
+  ADD PRIMARY KEY (`item_id_todolist`),
+  ADD KEY `Foregin_Key` (`user_id`);
+
+--
 -- Indexes for table `tutors`
 --
 ALTER TABLE `tutors`
@@ -218,8 +336,26 @@ ALTER TABLE `users`
   ADD KEY `foreign_key` (`type`);
 
 --
+-- Indexes for table `user_types`
+--
+ALTER TABLE `user_types`
+  ADD PRIMARY KEY (`type`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `booking_number` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `calendar`
+--
+ALTER TABLE `calendar`
+  MODIFY `item_id_calendar` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `courses`
@@ -231,19 +367,31 @@ ALTER TABLE `courses`
 -- AUTO_INCREMENT for table `pending_students`
 --
 ALTER TABLE `pending_students`
-  MODIFY `temp_student_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `temp_student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `pending_tutors`
 --
 ALTER TABLE `pending_tutors`
-  MODIFY `temp_user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `temp_user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
   MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `temp_codes`
+--
+ALTER TABLE `temp_codes`
+  MODIFY `code_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `to_do_list`
+--
+ALTER TABLE `to_do_list`
+  MODIFY `item_id_todolist` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tutors`
@@ -262,10 +410,36 @@ ALTER TABLE `users`
 --
 
 --
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `fk_foreign_key_name1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`),
+  ADD CONSTRAINT `fk_foreign_key_name3` FOREIGN KEY (`tutor_id`) REFERENCES `tutors` (`tutor_ID`),
+  ADD CONSTRAINT `fk_foreign_key_name4` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`);
+
+--
+-- Constraints for table `calendar`
+--
+ALTER TABLE `calendar`
+  ADD CONSTRAINT `calendar_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
 -- Constraints for table `students`
 --
 ALTER TABLE `students`
   ADD CONSTRAINT `fk_foreign_key_name` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `temp_codes`
+--
+ALTER TABLE `temp_codes`
+  ADD CONSTRAINT `temp_codes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `to_do_list`
+--
+ALTER TABLE `to_do_list`
+  ADD CONSTRAINT `Foregin_Key` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `tutors`
