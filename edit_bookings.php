1b5cf105826b4231d5a777fb5d85b72b135f9ba4
Ali Knayber <79058397+Ali-Knayber@users.noreply.github.com>
2021-12-07 16:14:07 +0200

    update_booking

---
+++ edit_bookings.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+include("connection.php");
+
+
+$student_id = $_POST["student"];
+$tutor_id = $_POST["tutor"];
+$course_id = $_POST["course"];
+$date = $_POST["date"];
+$days_of_sessions="";
+
+
+die($student_id);
+?>
